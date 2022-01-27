61e9218ed2620b4f94fad739f5034edd4257ae46
bpwoolfolk <bpwoolfolk@hotmail.com>
2022-01-26 20:38:08 -0500

    Initial commit.

---
+++ lsp-assignments/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	// 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
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!!");
+	}
+
+}
